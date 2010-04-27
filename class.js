47150934c1a6d083c1b4880fefb971616a7df34e
Robert Katic <robert.katic@gmail.com>
2010-04-27 15:57:19 +0200

    Added support for mixins.

--- class.js
+++ class.js
@@ -1,39 +1,57 @@
 (function(){
 
 	var F = function(){},
-		hasOwnProperty = Object.prototype.hasOwnProperty,
-		toString = Object.prototype.toString,
+		OP = Object.prototype,
+		hasOwnProperty = OP.hasOwnProperty,
+		toString = OP.toString,
 		rsuper = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /^/;
 	
 	function isFunction( obj ) {
-		return toString.call(obj) === "[object Function]";
+		return !!obj && toString.call(obj) === "[object Function]";
 	}
 	
-	function proxy( fn, parent, i ) {
+	function isArray( obj ) {
+		return !!obj && toString.call(obj) === "[object Array]";
+	}
+	
+	function proxy( fn, parent, method ) {
 		return function() {
 			var tmp = this._super;
-			this._super = i ? parent[i] : parent;
+			this._super = method ? parent[ method ] : parent;
 			var ret = fn.apply(this, arguments);
 			this._super = tmp;
 			return ret;
 		};
 	}
 	
-	function $object( proto, prop ) {
-		F.prototype = proto;
+	function $object( parent, mixins, prop ) {
+		F.prototype = parent || OP;
 		var obj = new F();
 		
+		if ( mixins && isArray(mixins) ) {
+			for ( var i = 0, l = mixins.length; i < l; ++i ) {
+				var mixin = mixins[i];
+				
+				for ( var key in mixin ) {
+					obj[ key ] = mixin[ key ];
+				}
+			}
+			
+		} else {
+			prop = prop || mixins;
+		}
+		
 		if ( prop ) {
-			for ( var i in prop ) {
+			for ( var key in prop ) {
 				// Own properties are enumerated firstly, so break if current one is not own.
-				if ( !hasOwnProperty.call(prop, i) ) {
+				if ( !hasOwnProperty.call(prop, key) ) {
 					break;
 				}
 				
-				var value = prop[i];
+				var value = prop[ key ];
 				
-				obj[i] = isFunction(value) && rsuper.test(value)  ?
-					proxy( value, proto, i ) :
+				obj[i] = isFunction( value ) && rsuper.test( value ) ?
+					proxy( value, parent, key ) :
 					value;
 			}
 		}
@@ -41,25 +59,26 @@
 		return obj;
 	}
 	
-	function $class( base, prop ) {        
-		if ( !isFunction(base) ) {
-			prop = base;
-			base = null;
-		}
+	function $class( /* base, mixins, prop */ ) {
+		var a = arguments, i = 0,
+			base = isFunction( a[i] ) ? a[i++] : null,
+			mixins = isArray( a[i] ) ? a[i++] : null,
+			prop = a[i],
+			cls = prop && hasOwnProperty.call( prop, "constructor" ) && prop.constructor;
 		
-		var cls = prop && hasOwnProperty.call(prop, "constructor") && isFunction(prop.constructor) ?
-			prop.constructor : null;
-		
-		if ( cls == null ) {
+		if ( !cls || !isFunction(cls) ) {
 			cls = base ?
 				function(){ return base.apply(this, arguments); } :
 				function(){};
 		
 		} else if ( rsuper.test(cls) ) {
-			cls = proxy(cls, base);
+			cls = proxy( cls, base );
 		}
 		
-		cls.prototype = base ? $object(base.prototype, prop) : prop;
+		cls.prototype = base || mixins && mixins.length ?
+			$object( base && base.prototype, mixins, prop ) :
+			prop;
+			
 		cls.prototype.constructor = cls;
 		
 		return cls;
@@ -68,5 +87,7 @@
 	//EXPOSE
 	this.$object = $object;
 	this.$class = $class;
+	//this.isFunction = isFunction;
+	//this.isArray = isArray;
 
-})();
+}).call(this);
