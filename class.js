17b35a56d42fc003e8275852cfa7279cb33d73dc
Robert Katic <robert.katic@gmail.com>
2010-09-11 08:57:43 +0200

    Simplified , removed usage of hasOwnProperty,...

--- class.js
+++ class.js
@@ -2,9 +2,10 @@
 
 	var F = function(){},
 		OP = Object.prototype,
-		hasOwnProperty = OP.hasOwnProperty,
 		ostr = OP.toString,
-		rsuper = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /^/;
+		tmp = function(){foo;},
+		foo = tmp, // IE9..
+		reSuper = /foo/.test( foo ) ? /\b_super\b/ : /^/;
 	
 	function isFunction( obj ) {
 		return !!obj && ostr.call(obj) === "[object Function]";
@@ -24,37 +25,18 @@
 		};
 	}
 	
-	function $object( parent, mixins, prop ) {
+	function extend( obj, mixin ) {
+		for ( var key in mixin ) {
+			obj[ key ] = mixin[ key ];
+		}
+		return obj;
+	}
+	
+	function $object( parent, mixin ) {
 		F.prototype = parent || OP;
 		var obj = new F();
 		
-		if ( mixins && isArray(mixins) ) {
-			for ( var i = 0, l = mixins.length; i < l; ++i ) {
-				var mixin = mixins[i];
-				
-				for ( var key in mixin ) {
-					obj[ key ] = mixin[ key ];
-				}
-			}
-			
-		} else {
-			prop = prop || mixins;
-		}
-		
-		if ( prop ) {
-			for ( var key in prop ) {
-				// Own properties are enumerated firstly, so break if current one is not own.
-				if ( !hasOwnProperty.call(prop, key) ) {
-					break;
-				}
-				
-				var value = prop[ key ];
-				
-				obj[i] = isFunction( value ) && rsuper.test( value ) ?
-					proxy( value, parent, key ) :
-					value;
-			}
-		}
+		mixin && extend( obj, mixin );
 		
 		return obj;
 	}
@@ -63,23 +45,46 @@
 		var a = arguments, i = 0,
 			base = isFunction( a[i] ) ? a[i++] : null,
 			mixins = isArray( a[i] ) ? a[i++] : null,
-			prop = a[i],
-			cls = prop && hasOwnProperty.call( prop, "constructor" ) && prop.constructor;
+			prop = a[i] || {},
+			parent = base && base.prototype,
+			cls, prototype;
 		
-		if ( !isFunction(cls) ) {
-			cls = base ?
-				function(){ return base.apply(this, arguments); } :
-				function(){};
-		
-		} else if ( rsuper.test(cls) ) {
-			cls = proxy( cls, base );
+		if ( base || mixins && mixins.length ) {
+			prototype = $object( parent );
+			
+			for ( var i = 0; i < mixins.length; ++i ) {
+				extend( prototype, mixins[i] );
+			}
+			
+			if ( prop ) {
+				for ( var name in prop ) {
+					var value = prop[ name ];
+					
+					if ( name === "constructor" && isFunction( value ) ) {
+						cls = base && reSuper.test( value ) ?
+							proxy( value, base ) :
+							value;
+							
+					} else {
+						prototype[ name ] = prototype[ name ] && isFunction( value ) && reSuper.test( value ) ?
+							proxy( value, parent, name ) :
+							value;
+					}
+				}
+			}
+			
+		} else {
+			prototype = prop;
 		}
 		
-		cls.prototype = base || mixins && mixins.length ?
-			$object( base && base.prototype, mixins, prop ) :
-			prop;
-			
-		cls.prototype.constructor = cls;
+		if ( !cls ) {
+			cls =  base ?
+				function(){ return base.apply(this, arguments); } :
+				function(){};
+		}
+		
+		prototype.constructor = cls;
+		cls.prototype = prototype;
 		
 		return cls;
 	}
